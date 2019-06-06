0f75265c210a0a0f1791a80b570f6458251bdc07
malydok <marek@devalchemist.com>
2019-06-06 22:06:23 +0200

    setup example pub/sub with Nes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import App from './App.svelte';
+import Nes from '@hapi/nes/lib/client';
+
+const client = new Nes.Client('ws://localhost:3000');
+const start = async () => {
+  await client.connect();
+  client.subscribe('/', update => {
+    console.log(update);
+  });
+};
+start();
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    name: 'world',
+  },
 });
 
 export default app;
