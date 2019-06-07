2d21261a9f1f7004b60d8bc082388980e58cb848
malydok <marek@devalchemist.com>
2019-06-07 11:19:22 +0200

    add example api use

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,54 @@
 <script>
-	export let name;
+  import { connect } from "./api/api";
+  import { createRoom, connectToRoom } from "./api/room";
+
+  let connecting = true;
+  async function connectToApi() {
+    await connect();
+    connecting = false;
+  }
+  connectToApi();
+
+  let creatingRoom = false;
+  let roomId;
+  let roomMessages = [];
+
+  function enterRoom(roomId) {
+    connectToRoom(roomId, message => {
+      roomMessages = [...roomMessages, JSON.stringify(message)];
+    });
+  }
+
+  async function createRoomHandler() {
+    creatingRoom = true;
+    roomId = await createRoom();
+    creatingRoom = false;
+    enterRoom(roomId);
+  }
+
+  let roomInput = "";
+  async function enterRoomHandler() {
+    roomId = roomInput;
+    enterRoom(roomId);
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+{#if connecting}
+  <p>Connecting</p>
+{:else}
+  {#if roomId}
+    <p>Your room is {roomId}</p>
+    <ol>
+      {#each roomMessages as message}
+        <li>{message}</li>
+      {/each}
+    </ol>
+  {:else}
+    <button on:click={createRoomHandler} disabled={creatingRoom}>
+       {creatingRoom ? 'Creating' : 'Create'} a room
+    </button>
+    or
+    <input type="text" placeholder="Paste room ID" bind:value={roomInput} />
+    <button on:click={enterRoomHandler}>Enter room</button>
+  {/if}
+{/if}
