f5ff507e6994c216e34859a10553c52b92d7fc5b
devalche <marek@devalchemist.com>
2020-01-28 19:53:49 +0100

    setup api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,8 @@
 import Nes from '@hapi/nes/lib/client';
 
-export const api = new Nes.Client('ws://localhost:3000');
+const api_url =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api-hex.herokuapp.com'
+    : 'ws://localhost:3000';
+
+export const api = new Nes.Client(api_url);
